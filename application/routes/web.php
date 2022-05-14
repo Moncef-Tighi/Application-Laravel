1b5d17fb139f4381cae9030ac108364a120e7f6a
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-14 16:22:14 +0100

    Création des controllers de post avec resource

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,38 +19,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class, "home"])->name("home.index");
 Route::get('/contact', [HomeController::class, "contact"])->name("home.contact");
+Route::get('/about', [AboutController::class])->name("home.contact");
 
-$posts = [
-    1=> [
-        "title" => 'Into to Laravel',
-        "content" => "This is a short intro to Laravel",
-        "is_new" => true
-    ],
-    2=> [
-        "title" => "Into to PhP",
-        "content" => "This is a short intro to PHP",
-        "is_new" => false
-    ]
-];
 
-Route::prefix('/posts')->name('posts.')->group(function() use($posts){
+// Route::prefix('/posts')->name('posts.')->group(function() use($posts){
 
-    Route::get('/', function(Request $request) use($posts) {
-        dd($request->query());
-        return view('posts.liste', ["posts" => $posts]);
-    })->name('liste');
+//     Route::get('/', function(Request $request) use($posts) {
+//         dd($request->query());
+//         return view('posts.liste', ["posts" => $posts]);
+//     })->name('liste');
     
-    Route::get('/{id}', function($id) use($posts) {
+//     Route::get('/{id}', function($id) use($posts) {
     
-        abort_if(!isset($posts[$id]), 404);
+//         abort_if(!isset($posts[$id]), 404);
     
-        return view('posts.show', ["post" => $posts[$id]]);
-    })->name('show');
-});
+//         return view('posts.show', ["post" => $posts[$id]]);
+//     })->name('show');
+// });
+// Route::get('/responses', function() use($posts){
+//     return response() 
+//     ->json($posts)
+//     ->cookie('COOKIE', 'Test', 3600);
+// })->name('responses');
 
-Route::get('/responses', function() use($posts){
-    return response() 
-    ->json($posts)
-    ->cookie('COOKIE', 'Test', 3600);
-})->name('responses');
+
+Route::resource('/posts', PostController::class)->only(["index", "show"]);
+
+
 
